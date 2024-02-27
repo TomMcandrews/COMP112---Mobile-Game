7b706485cef15809393588d40dffaf39c24d5f28
TomMcandrews <146433954+TomMcandrews@users.noreply.github.com>
2024-02-27 14:51:17 +0000

    First commit

---
+++ Assets/gyro_rotation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gyro_rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.gyro.attitude);
+        transform.rotation = Input.gyro.attitude;
+    }
+}
